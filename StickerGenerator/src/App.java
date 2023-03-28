7e7de7614ca16538ba2f0097491c8fbece434c5e
Vinicius Miguel <95515458+ViniciusMiguell@users.noreply.github.com>
2023-03-28 14:44:35 -0300

    finished

--- StickerGenerator/src/App.java
+++ StickerGenerator/src/App.java
@@ -1,39 +1,62 @@
-import java.io.IOException;
+import javax.swing.JOptionPane;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class App {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws Exception {
 		
-		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
-		var client = HttpClient.newHttpClient();
-		var endereco = URI.create(url);
-		var request = HttpRequest.newBuilder(endereco).GET().build();
-		var response = client.send(request, BodyHandlers.ofString());
-		String body = response.body();
-		
-		var parser = new JsonParser();
-		List<Map<String, String>>listaDeFilmes = parser.parse(body);
-		
-		for (Map<String, String> filme : listaDeFilmes) {
-			System.out.println("\033[32;1mTitulo: \u001b[m"+filme.get("title"));
-			double classificação = Double.parseDouble(filme.get("imDbRating"));
-			int estrelas=(int) classificação;
-			for (int i=1;i<=estrelas;i++){
-				if (i==1){
-					System.out.print("\033[32;1mRating: \u001b[m("+filme.get("imDbRating")+")⭐");
-				} else{
-					System.out.print("⭐");
-				}
+		String[] opc = {"Stickers de Series", "Seu Sticker", "Sair"};
+		var Generator = new GeradorDeFigurinhas();
+		while (true){
+
+			char rep=0;
+			int selectedOpc = JOptionPane.showOptionDialog(null, "Escolha uma opção:", "Generator de Stickers", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opc, opc[0]);
+
+			switch (selectedOpc){
+				case 0:
+					String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+					var client = HttpClient.newHttpClient();
+					var address = URI.create(url);
+					var request = HttpRequest.newBuilder(address).GET().build();
+					var response = client.send(request, BodyHandlers.ofString());
+					String body = response.body();
+					
+					var parser = new JsonParser();
+					List<Map<String, String>>listOfMovies = parser.parse(body);
+
+					for (Map<String, String> film : listOfMovies) {
+						double classification = Double.parseDouble(film.get("imDbRating"));
+						String txt01="";
+						if (classification>=8.00){
+							txt01 = "Otimo Filme!!!";
+						} else if(classification>=6.00 && classification<8.00){
+							txt01 = "Bom Filme!!";
+						} else{
+							txt01 = "Péssimo Filme!";
+						}
+						String fileName = film.get("title").replace(":", "-");
+						Generator.cria(film.get("image"), txt01, fileName, false);
+					}
+					break;
+
+				case 1:
+					String urlImg=JOptionPane.showInputDialog(null, "Insira a url da imagem: ");
+					String txt02=JOptionPane.showInputDialog(null, "Insira o texto da figurinha: ");
+					Generator.cria(urlImg, txt02, "figurinha", true);
+					break;
+
+				case 2:
+					rep=1;
+					break;
 			}
-			System.out.println("\n\033[32;1mImagem: \u001b[m"+filme.get("image"));
-			System.out.println("\n");
+			if (rep==1) {
+				break;
+			}
 		}
 	}
 }
--- StickerGenerator/src/GeradorDeFigurinhas.java
+++ StickerGenerator/src/GeradorDeFigurinhas.java
@@ -1,3 +1,58 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.TextLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
 public class GeradorDeFigurinhas {
     
+    public void cria(String url, String text, String fileName, boolean stickerPersonalised) throws Exception{
+        // Leitura da imagem
+        InputStream inputStream = new URL(url).openStream();
+        BufferedImage image = ImageIO.read(inputStream);
+        
+        int width = image.getWidth();
+        int height = (int) (image.getHeight() * 1.1);
+        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
+
+        if (stickerPersonalised== true){
+            width = image.getWidth();
+            height = (int) (image.getHeight() * 1.2);
+            newImage = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
+        }
+
+        Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+        graphics.drawImage(image, 0, 0, null);
+
+        var font = new Font(Font.SANS_SERIF, Font.BOLD, (width / 8));
+        graphics.setColor(Color.GREEN);
+        graphics.setFont(font);
+
+        FontMetrics fm = graphics.getFontMetrics();
+        int x = (width - fm.stringWidth(text)) / 2;
+
+        graphics.drawString(text, x, (int) (height * 0.985));
+
+        // Drawing outline
+        TextLayout t1 = new TextLayout(text, font, graphics.getFontRenderContext());
+        Shape shape = t1.getOutline(null);
+
+        graphics.translate(x, (int) (height * 0.985));
+        graphics.setColor(Color.BLACK);
+        graphics.setStroke(new BasicStroke((int) (width / 216)));
+        graphics.draw(shape);
+
+
+        // Escrever a nova imagem em um arquivo
+        var Directory = new File("figurinhas/");
+        Directory.mkdir();
+        ImageIO.write(newImage, "png", new File("figurinhas/"+fileName+".png"));
+    }
 }
